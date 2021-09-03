3125baa61eb9a648c300863f9757515b49a72c99
Ciara Swann <cccswann@gmail.com>
2021-09-02 20:38:53 -0700

    Update jobs list component.

--- src/components/JobsList.jsx
+++ src/components/JobsList.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux'
 import JobItem from './JobItem';
 
 
 const JobsList = ({ jobs }) => {
+
     return (
         <div>
             <h3>Jobs List:</h3>
+            <input
+				type='text'
+				class='form-control mb-2 mr-sm-2'
+				placeholder='Type to search...'
+			/>
             {jobs.jobs.map(job => 
                 <ul key={job.id} className='list-group'>
                     <JobItem id={job.id} title={job.title} company={job.company} status={job.status}/>
